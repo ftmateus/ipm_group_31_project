9d9821c9aa2f5c782409da5ba8ac1a67ba2e10eb
Francisco Mateus <ft.mateus@campus.fct.unl.pt>
2022-09-22 14:09:44 +0100

    Navbar

--- project_website/src/App.tsx
+++ project_website/src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import NavDropdown from 'react-bootstrap/NavDropdown'
+// import Nav from 'react-bootstrap/Nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+        
+        {/* <div style={{display : 'flex', width : 750, justifyContent : 'space-evenly', alignItems : 'center', fontSize : 25}}>
+            <a>Assignments</a>
+            <a>Project</a>
+            <a>About us</a>
+        </div> */}
+        
+        <Navbar.Brand href="#home">
+          <img src={logo} className="App-logo" alt="logo" />
+          IPM Project group 31
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="justify-content-end" activeKey="/home">
+              {/* <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+              </Nav.Item> */}
+              <NavDropdown title="Assignments" >
+                <NavDropdown.Item eventKey="link-1">Assignment 1</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Project">
+                <Nav.Link eventKey="link-2">Project</Nav.Link>
+              </NavDropdown>
+              <Nav title="About us">
+                <Nav.Link eventKey="link-2">About us</Nav.Link>
+              </Nav>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
