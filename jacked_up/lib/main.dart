0bd771cf34b1f23ba18f316406d12b19121bfbd6
Francisco Mateus <ft.mateus@campus.fct.unl.pt>
2022-11-16 14:16:22 +0000

    Initial files

--- jacked_up/lib/main.dart
+++ jacked_up/lib/main.dart
@@ -1,115 +1,188 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
 
-void main() {
-  runApp(const MyApp());
+class BottomAppBarDemo extends StatefulWidget {
+  const BottomAppBarDemo({Key? key}) : super(key: key);
+
+  @override
+  State createState() => _BottomAppBarDemoState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _BottomAppBarDemoState extends State<BottomAppBarDemo>
+    with RestorationMixin {
+  final RestorableBool _showFab = RestorableBool(true);
+  final RestorableBool _showNotch = RestorableBool(true);
+  final RestorableInt _currentFabLocation = RestorableInt(0);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  String get restorationId => 'bottom_app_bar_demo';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_showFab, 'show_fab');
+    registerForRestoration(_showNotch, 'show_notch');
+    registerForRestoration(_currentFabLocation, 'fab_location');
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void dispose() {
+    _showFab.dispose();
+    _showNotch.dispose();
+    _currentFabLocation.dispose();
+    super.dispose();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Since FloatingActionButtonLocation is not an enum, the index of the
+  // selected FloatingActionButtonLocation is used for state restoration.
+  static const List<FloatingActionButtonLocation> _fabLocations = [
+    FloatingActionButtonLocation.endDocked,
+    FloatingActionButtonLocation.centerDocked,
+    FloatingActionButtonLocation.endFloat,
+    FloatingActionButtonLocation.centerFloat,
+  ];
 
-  void _incrementCounter() {
+  void _onShowNotchChanged(bool value) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _showNotch.value = value;
+    });
+  }
+
+  void _onShowFabChanged(bool value) {
+    setState(() {
+      _showFab.value = value;
+    });
+  }
+
+  void _onFabLocationChanged(int? value) {
+    setState(() {
+      _currentFabLocation.value = value!;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final localizations = GalleryLocalizations.of(context)!;
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        automaticallyImplyLeading: false,
+        title: Text(localizations.demoBottomAppBarTitle),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        padding: const EdgeInsets.only(bottom: 88),
+        children: [
+          SwitchListTile(
+            title: Text(
+              localizations.demoFloatingButtonTitle,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            value: _showFab.value,
+            onChanged: _onShowFabChanged,
+          ),
+          SwitchListTile(
+            title: Text(localizations.bottomAppBarNotch),
+            value: _showNotch.value,
+            onChanged: _onShowNotchChanged,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(localizations.bottomAppBarPosition),
+          ),
+          RadioListTile<int>(
+            title: Text(
+              localizations.bottomAppBarPositionDockedEnd,
+            ),
+            value: 0,
+            groupValue: _currentFabLocation.value,
+            onChanged: _onFabLocationChanged,
+          ),
+          RadioListTile<int>(
+            title: Text(
+              localizations.bottomAppBarPositionDockedCenter,
+            ),
+            value: 1,
+            groupValue: _currentFabLocation.value,
+            onChanged: _onFabLocationChanged,
+          ),
+          RadioListTile<int>(
+            title: Text(
+              localizations.bottomAppBarPositionFloatingEnd,
+            ),
+            value: 2,
+            groupValue: _currentFabLocation.value,
+            onChanged: _onFabLocationChanged,
+          ),
+          RadioListTile<int>(
+            title: Text(
+              localizations.bottomAppBarPositionFloatingCenter,
+            ),
+            value: 3,
+            groupValue: _currentFabLocation.value,
+            onChanged: _onFabLocationChanged,
+          ),
+        ],
+      ),
+      floatingActionButton: _showFab.value
+          ? FloatingActionButton(
+        onPressed: () {},
+        tooltip: localizations.buttonTextCreate,
+        child: const Icon(Icons.add),
+      )
+          : null,
+      floatingActionButtonLocation: _fabLocations[_currentFabLocation.value],
+      bottomNavigationBar: _DemoBottomAppBar(
+        fabLocation: _fabLocations[_currentFabLocation.value],
+        shape: _showNotch.value ? const CircularNotchedRectangle() : null,
+      ),
+    );
+  }
+}
+
+class _DemoBottomAppBar extends StatelessWidget {
+  const _DemoBottomAppBar({
+    required this.fabLocation,
+    this.shape,
+  });
+
+  final FloatingActionButtonLocation fabLocation;
+  final NotchedShape? shape;
+
+  static final centerLocations = <FloatingActionButtonLocation>[
+    FloatingActionButtonLocation.centerDocked,
+    FloatingActionButtonLocation.centerFloat,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final localizations = GalleryLocalizations.of(context)!;
+    return BottomAppBar(
+      shape: shape,
+      child: IconTheme(
+        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+        child: Row(
+          children: [
+            IconButton(
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              icon: const Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            if (centerLocations.contains(fabLocation)) const Spacer(),
+            IconButton(
+              tooltip: localizations.starterAppTooltipSearch,
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              tooltip: localizations.starterAppTooltipFavorite,
+              icon: const Icon(Icons.favorite),
+              onPressed: () {},
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
