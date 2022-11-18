b377d2f99329b6c5f4c249cc8de7dce408b25eb0
Bruno <bm.carmo@campus.fct.unl.pt>
2022-11-18 15:17:52 +0000

    Added the register screen

--- jacked_up/lib/SignInScreen.dart
+++ jacked_up/lib/SignInScreen.dart
@@ -34,6 +34,98 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  userNameField() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        hintText: 'Enter your username',
+        labelText: 'Username',
+        icon: Icon(Icons.person),
+      ),
+      validator: (String? value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter an username';
+        }
+        return null;
+      },
+    );
+  }
+
+  passwordField() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        hintText: 'Enter your password',
+        labelText: 'Password',
+        icon: Icon(Icons.key),
+      ),
+      validator: (String? value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a password';
+        }
+        return null;
+      },
+    );
+  }
+
+  signInButton() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(30),
+      child: Stack(
+        children: <Widget>[
+          Positioned.fill(
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFF0D47A1),
+                    Color(0xFF1976D2),
+                    Color(0xFF42A5F5),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.only(
+                  left: 40, top: 16, bottom: 16, right: 40),
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Data is in processing.')));
+              }
+            },
+            child: const Text('Sign In'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  createAnAccontText() {
+    return RichText(
+      text: TextSpan(children: [
+        const TextSpan(
+          text: 'Don\'t have an account? ',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        TextSpan(
+            text: 'Sign up',
+            style: const TextStyle(
+              color: Colors.blue,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                Navigator.pushNamed(context, RegisterScreen.id);
+              }),
+      ]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,103 +135,28 @@ class _SignInScreenState extends State<SignInScreen> {
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                const Expanded(
-                  child: Image(
-                  image: AssetImage('assets/images/jackedUp.png'),
-                  fit: BoxFit.cover,
-                  width: double.infinity)),
+              const Expanded(
+                child: Image(
+                image: AssetImage('assets/images/jackedUp.png'),
+                fit: BoxFit.cover,
+                width: double.infinity)),
               Form(
-                key: _formKey,
+              key: _formKey,
                 child: SizedBox(
                   width: 420,
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      TextFormField(
-                      decoration: const InputDecoration(
-                      hintText: 'Enter your username',
-                      labelText: 'Username',
-                      icon: Icon(Icons.person),
-                      ),validator:(String? value){
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter an username';
-                        }
-                        return null;
-                      },
-                      ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                      hintText: 'Enter your password',
-                      labelText: 'Password',
-                      icon: Icon(Icons.key),
-                  ),validator:(String? value){
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a password';
-                      }
-                      return null;
-                    },
-              ),
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                  userNameField(),
+                  passwordField(),
                     const SizedBox(height: 30),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child: Stack(
-                          children: <Widget>[
-                            Positioned.fill(
-                              child: Container(
-                              decoration: const BoxDecoration(
-                              gradient: LinearGradient(
-                              colors: <Color>[
-                                Color(0xFF0D47A1),
-                                Color(0xFF1976D2),
-                                Color(0xFF42A5F5),
-                              ],
-                              ),
-                            ),
-                          ),
-                        ),
-                          TextButton(
-                            style: TextButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            padding: const EdgeInsets.only(
-                              left: 40, top: 16, bottom: 16, right: 40),
-                            textStyle: const TextStyle(fontSize: 20),
-                            ),
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data is in processing.')));
-                              }
-                            },
-                            child: const Text('Sign In'),
-                      ),
-                    ],
-                  ),
-              ),
-            ],
+                  signInButton(),
+              ],
+            ),
           ),
-                ),
         ),
-                const SizedBox(height: 15),
-
-                RichText(
-                text: TextSpan(children: [
-                const TextSpan(
-                  text: 'Don\'t have an account? ',
-                  style: TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-                  TextSpan(
-                  text: 'Sign up',
-                  style: const TextStyle(
-                  color: Colors.blue,
-                  ),
-                  recognizer: TapGestureRecognizer()
-                  ..onTap = () {
-                    Navigator.pushNamed(context, RegisterScreen.id);
-                  }
-                  ),
-          ]),
-        ),
+              const SizedBox(height: 15),
+              createAnAccontText(),
               const SizedBox(height: 120),
       ],
     )));
