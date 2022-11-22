75b40c1f96c5092ab1800d3b232ba01ad3ca4646
Francisco Mateus <ft.mateus@campus.fct.unl.pt>
2022-11-22 01:34:03 +0000

    Rename main.dart to MainMenu.dart

--- jacked_up/test/widget_test.dart
+++ jacked_up/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:jacked_up/main.dart';
+import 'package:jacked_up/MainMenu.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
